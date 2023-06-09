979ca93b3e45433f7e423f4dd87ee329587381d1
Asgysygs <you@example.com>
2023-06-09 11:53:25 +0300

    add eslint

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-const cosl = document.querySelectorAll('col') // селектор из верстки , возможно  измениться название 
- 
+const cosl = document.querySelectorAll('.column');
 
-function generateRandomColor() {
-    const hexCodes ='0123456789ABCDEF'
-    let color  = ''
-    for ( let i = 0 ; i < 6; i++) {
-        color += hexCodes[Math.floor(Math.random() *  hexCodes.length)] //  код для получения  рандомных цвета 
-    }
-    return '#' + color
-}
+const generateRandomColor = () => {
+  const hexCodes = '0123456789ABCDEF';
+  let color = '';
+  for (let i = 0; i < 6; i += 1) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  return `#${color}`;
+};
 
-function getRandomColors(){
-    cosl.forEach( (col ) => {
-        const text  = col.querySelector('h2')
-        const color = generateRandomColor()
-        text.textContent= color  // цифры с цветами  
-      col.style.background = color  //  код для изменения  цвета на фоне   
-    } )
-    }
-    function 
-getRandomColors()
- 
+const getRandomColors = () => {
+  cosl.forEach((column) => {
+    column.style.background = generateRandomColor();
+  });
+};
+getRandomColors();
