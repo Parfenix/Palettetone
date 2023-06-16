8b44f345ac2ba47bd27a4f32447aeadc6cb1f939
Denis Parfenov <Parfenix@gmail.com>
2023-06-16 11:54:39 +0300

    added a space button and a non-working script to change the font color

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import chroma from "chroma-js";
-
 const columnl = document.querySelectorAll('.column');
 const generateRandomColor = () => {
   const hexCodes = '0123456789ABCDEF';
@@ -9,13 +7,29 @@ const generateRandomColor = () => {
   }
   return `#${color}`;
 };
+
+document.addEventListener('keydown', (event) => {
+  if (event.code.toLowerCase() === 'space') {
+    getRandomColors();
+  };
+});
+
 const getRandomColors = () => {
   columnl.forEach((column) => {
     const text = column.querySelector('h2');
+    const buttons = column.querySelector("button");
     const color = generateRandomColor();
     text.textContent = color;
     column.style.background = generateRandomColor();
+
+    setTextColor(text, color);
+    setTextColor(buttons, color);
   });
+};
+
+const setTextColor = (text, color) => {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? 'black' : 'white';
 };
 
 getRandomColors();
