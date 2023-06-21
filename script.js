023e20bd6b7fbd0198d3e8aaf2ebe322b43ac272
coco1508 <kostya.petryaev114@gmail.com>
2023-06-21 12:36:03 +0300

    change text color

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const columnl = document.querySelectorAll('.column');
-const generateRandomColor = () => {
-  const hexCodes = '0123456789ABCDEF';
-  let color = '';
-  for (let i = 0; i < 6; i += 1) {
-    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
-  }
-  return `#${color}`;
-};
+
 
 document.addEventListener('keydown', (event) => {
   if (event.code.toLowerCase() === 'space') {
@@ -14,18 +7,22 @@ document.addEventListener('keydown', (event) => {
   };
 });
 
+
+
 const getRandomColors = () => {
   columnl.forEach((column) => {
     const text = column.querySelector('h2');
-    const buttons = column.querySelector("button");
-    const color = generateRandomColor();
+    const button = column.querySelector("button");
+    const color = chroma.random();
     text.textContent = color;
-    column.style.background = generateRandomColor();
+    column.style.background = color;
+
 
     setTextColor(text, color);
-    setTextColor(buttons, color);
+    setTextColor(button, color);
   });
 };
+
 
 const setTextColor = (text, color) => {
   const luminance = chroma(color).luminance();
