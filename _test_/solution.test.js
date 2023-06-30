15f7b5331f55a91938227eab15e9ea68cd354e29
Parfenix <Parfenix@gmail.com>
2023-06-30 12:30:23 +0300

    fixed some eslint problems

--- _test_/solution.test.js
+++ _test_/solution.test.js
@@ -1,34 +1,34 @@
 function getRandomColors() {
-    const hexCodes ='0123456789ABCDEF'
-    let color  = ''
-    for ( let i = 0 ; i < 6; i++) {
-            color += hexCodes[Math.floor(Math.random() *  hexCodes.length)] //  код для получения  рандомных цвета 
-    }
-    return '#' + color
-    // Функция генерации случайного цвета.
-};
+  const hexCodes = '0123456789ABCDEF';
+  let color = '';
+  for (let i = 0; i < 6; i += 1) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  return '#' + color;
+  // Функция генерации случайного цвета.
+}
 
-  // Генерируем случайные цвета
+// Генерируем случайные цвета
 const colors = getRandomColors();
 
-  // Проверяем, что массив цветов не пустой
+// Проверяем, что массив цветов не пустой
 if (!colors || colors.length === 0) {
-    throw new Error('Массив цветов пуст');
-};
+  throw new Error('Массив цветов пуст');
+}
 
-  // Проверяем, что все цвета в массиве действительные
+// Проверяем, что все цвета в массиве действительные
 for (const color of colors) {
-    if (!isValidColor(color)) {
-        throw new Error(`Неверный цвет: ${color}`);
-    }
-};
+  if (!isValidColor(color)) {
+    throw new Error(`Неверный цвет: ${color}`);
+  }
+}
 
-  // Проверяем, что каждый цвет не является похожим на предыдущий
-for (let i = 1; i < colors.length; i++) {
-    const previousColor = colors[i - 1];
-    const currentColor = colors[i];
+// Проверяем, что каждый цвет не является похожим на предыдущий
+for (let i = 1; i < colors.length; i += 1) {
+  const previousColor = colors[i - 1];
+  const currentColor = colors[i];
 
-    if (areColorsSimilar(previousColor, currentColor)) {
-        throw new Error('Сгенерирован похожий цвет');
-    }
-};
+  if (areColorsSimilar(previousColor, currentColor)) {
+    throw new Error('Сгенерирован похожий цвет');
+  }
+}
